ecf9226587d61b8c69723495d96edc8925530ec0
= <epautsch@luc.edu>
2022-12-12 18:21:02 -0600

    Console input supported

--- main.py
+++ main.py
@@ -8,12 +8,21 @@ import Evaluate as eval
 import matplotlib.pyplot as plt
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+input_lang, output_lang, pairs = dp.prepareData('eng', 'spa', True)
 
 try:
-    encoder1 = torch.load('encoder.pth')
-    attn_decoder1 = torch.load('attn_decoder1.pth')
+    encoder1 = torch.load('encoder.pth', map_location=torch.device('cpu'))
+    attn_decoder1 = torch.load('attn_decoder1.pth', map_location=torch.device('cpu'))
+    # eval.evaluateAndShowAttention('Cada vez que escucho esta canción, lloro.', encoder1, attn_decoder1, input_lang, output_lang)
+    user_input = input('Input sentence to be translated: ')
+    while user_input != 'exit' or 'Exit':
+        output_words, attentions = eval.evaluate(encoder1, attn_decoder1, user_input, input_lang, output_lang)
+        output_sentence = ' '.join(output_words)
+        print(output_sentence)
+        user_input = input('Input sentence to be translated: ')
+
 except FileNotFoundError:
-    input_lang, output_lang, pairs = dp.prepareData('eng', 'spa', True)
+
     hidden_size = 256
     encoder1 = net.EncoderRNN(input_lang.n_words, hidden_size).to(device)
     attn_decoder1 = net.AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
