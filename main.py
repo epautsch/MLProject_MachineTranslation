794f88d145977e5a621702a8a5bb698697c7cccb
labUser <epautsch@luc.edu>
2022-12-13 03:59:38 +0000

    Trained on vm

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ import Evaluate as eval
 import matplotlib.pyplot as plt
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 input_lang, output_lang, pairs = dp.prepareData('eng', 'spa', True)
 
 try:
-    encoder1 = torch.load('encoder_lstm.pth', map_location=torch.device('cpu'))
-    attn_decoder1 = torch.load('attn_decoder1_lstm.pth', map_location=torch.device('cpu'))
+    # encoder1 = torch.load('encoder_lstm.pth', map_location=torch.device('cpu'))
+    # attn_decoder1 = torch.load('attn_decoder1_lstm.pth', map_location=torch.device('cpu'))
+    encoder1 = torch.load('encoder_lstm.pth')
+    attn_decoder1 = torch.load('attn_decoder1_lstm.pth')
     # eval.evaluateAndShowAttention('Cada vez que escucho esta canción, lloro.', encoder1, attn_decoder1, input_lang, output_lang)
     user_input = input('Input sentence to be translated: ')
     while user_input != 'exit' or 'Exit':
@@ -26,7 +29,7 @@ except FileNotFoundError:
     hidden_size = 256
     encoder1 = net.EncoderRNN(input_lang.n_words, hidden_size).to(device)
     attn_decoder1 = net.AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
-    trn.trainIters(encoder1, attn_decoder1, 75, pairs, input_lang, output_lang, print_every=100)
+    trn.trainIters(encoder1, attn_decoder1, 150000, pairs, input_lang, output_lang, print_every=100)
     print(random.choice(pairs))
     eval.evaluateRandomly(encoder1, attn_decoder1, pairs, input_lang, output_lang)
     torch.save(encoder1, 'encoder_lstm.pth')
