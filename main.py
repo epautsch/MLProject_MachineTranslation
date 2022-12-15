c92ef3f40a46fb27dc54707d3d0129fbada8e1a7
labUser <epautsch@luc.edu>
2022-12-15 03:13:19 +0000

    trained on vm for gru_6L_tf0_2

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ print(device)
 input_lang, output_lang, pairs = dp.prepareData('eng', 'spa', True)
 
 try:
-    # encoder1 = torch.load('encoder_gru_6L_tf0_8.pth', map_location=torch.device('cpu'))
-    # attn_decoder1 = torch.load('attn_decoder1_gru_6L_tf0_8.pth', map_location=torch.device('cpu'))
-    encoder1 = torch.load('encoder_gru_6L_tf0_8.pth')
-    attn_decoder1 = torch.load('attn_decoder1_gru_6L_tf0_8.pth')
+    # encoder1 = torch.load('encoder_gru_6L_tf0_2.pth', map_location=torch.device('cpu'))
+    # attn_decoder1 = torch.load('attn_decoder1_gru_6L_tf0_2.pth', map_location=torch.device('cpu'))
+    encoder1 = torch.load('encoder_gru_6L_tf0_2.pth')
+    attn_decoder1 = torch.load('attn_decoder1_gru_6L_tf0_2.pth')
     # eval.evaluateAndShowAttention('Cada vez que escucho esta canción, lloro.', encoder1, attn_decoder1, input_lang, output_lang)
     user_input = input('Input sentence to be translated: ')
     while user_input.lower() != 'exit':
@@ -36,8 +36,8 @@ except FileNotFoundError:
     trn.trainIters(encoder1, attn_decoder1, 450000, pairs, input_lang, output_lang, print_every=1000)
     print(random.choice(pairs))
     eval.evaluateRandomly(encoder1, attn_decoder1, pairs, input_lang, output_lang)
-    torch.save(encoder1, 'encoder_gru_6L_tf0_8.pth')
-    torch.save(attn_decoder1, 'attn_decoder1_gru_6L_tf0_8.pth')
+    torch.save(encoder1, 'encoder_gru_6L_tf0_2.pth')
+    torch.save(attn_decoder1, 'attn_decoder1_gru_6L_tf0_2.pth')
     output_words, attentions = eval.evaluate(encoder1, attn_decoder1, "Mi nombre es .", input_lang, output_lang)
     plt.matshow(attentions.numpy())
 
